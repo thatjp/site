0f4c5d40dca45ab1d1729621ca08b79a6d950539
thatjp <jpharris.contact@gmail.com>
2018-11-15 23:31:57 -0500

    + downloadable pdf resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,55 +1,16 @@
 import React, { Component } from 'react';
 
-import { Document, Page } from 'react-pdf';
-
 class Resume extends Component {
-
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      pageNumber: 1
-    }
-  }
-
-  onDocumentLoad = ({ numPages }) => {
-    this.setState({ numPages });
-  }
-
-  isShowing = () => {
-    const { pageNumber }= this.state
-    if(this.props.isOpen){
-      return (
-        <div>          
-          <Document
-            file={require("../assets/jp-harris-resume.pdf")}
-            onLoadSuccess={this.onDocumentLoad}
-            // className='resume-image'
-          >
-            <Page pageNumber={pageNumber} />
-          </Document>
-        </div>
-      )
-    }
-  }
-
-  showResume = () => {
-    this.props.onClick('resume')
-  }
-
   render() {
     return (
       <div>
-        <div>
-          <h1 
-            onClick={this.showResume}
-          >
+        <a 
+         href={require("../assets/jp-harris-resume.pdf")} download
+        >
+          <h1>
             Resume
           </h1>
-        </div>
-        <div>
-          {this.isShowing()}
-        </div>
+        </a>
       </div>
     );
   }
