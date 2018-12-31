5d5af3d514f6bc1cfea366f5fd507d1cc7b65cb1
thatjp <jpharris.contact@gmail.com>
2018-12-31 14:04:51 -0500

    + spelling fixes

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,7 @@ import NavContentP from './styles/elements/NavContentP';
 const About = () => (
   <FadeAnimation>
     <NavContentP about>
-    Hi I&apos;m JP! A Creative front end web developer with background in art
-    and design. I bring a inventive attitude and open mind
-    to every project. My past work experience has given me
-    strong team and project management skills. In my free time,
+    Hi I&apos;m JP! A Creative front-end web developer with a background in art and design. I bring an inventive attitude and open mind to every project. My past work experience has given me a strong team and project management skills. In my free time,
     I work on music production and creative tech projects.
     </NavContentP>
   </FadeAnimation>
